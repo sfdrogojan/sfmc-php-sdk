fee0b01a31e4be594ee3c71e2fb352eb6f3745f0
sfadincescu <adincescu@salesforce.com>
2019-08-06 12:32:15 +0300

    Moved files to a different folder

---
+++ SalesForce/lib/Authorization/AuthService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SalesForce\MarketingCloud\Authorization;
+
+use SalesForce\MarketingCloud\Cache\CacheAwareInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use League\OAuth2\Client\Provider\GenericProvider;
+
+/**
+ * Class AuthService
+ *
+ * @package SalesForce\MarketingCloud\Authorization
+ */
+class AuthService implements CacheAwareInterface
+{
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+
+    /**
+     * @var GenericProvider
+     */
+    private $client;
+
+    /**
+     * Sets the cache
+     *
+     * @param CacheItemPoolInterface $cache
+     * @return mixed
+     */
+    public function setCache(CacheItemPoolInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Sets the client used for the authorization
+     *
+     * @param GenericProvider $client
+     */
+    public function setClient(GenericProvider $client): void
+    {
+        $this->client = $client;
+    }
+}
