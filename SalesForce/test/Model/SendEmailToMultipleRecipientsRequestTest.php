cbb83c7b0f5d8a278680581b9921d91d48cfa4f3
sfadincescu <adincescu@salesforce.com>
2019-08-16 14:46:07 +0300

    Regenerated the code Updated the PHPUnit XML file

--- SalesForce/test/Model/SendEmailToMultipleRecipientsRequestTest.php
+++ SalesForce/test/Model/SendEmailToMultipleRecipientsRequestTest.php
@@ -27,7 +27,9 @@
  * Please update the test case below to test the model.
  */
 
-namespace SalesForce\MarketingCloud;
+namespace SalesForce\MarketingCloud\Test\Model;
+
+use PHPUnit\Framework\TestCase;
 
 /**
  * SendEmailToMultipleRecipientsRequestTest Class Doc Comment
@@ -38,37 +40,8 @@ namespace SalesForce\MarketingCloud;
  * @author      Swagger Codegen team
  * @link        https://github.com/swagger-api/swagger-codegen
  */
-class SendEmailToMultipleRecipientsRequestTest extends \PHPUnit_Framework_TestCase
+class SendEmailToMultipleRecipientsRequestTest extends TestCase
 {
-
-    /**
-     * Setup before running any test case
-     */
-    public static function setUpBeforeClass()
-    {
-    }
-
-    /**
-     * Setup before running each test case
-     */
-    public function setUp()
-    {
-    }
-
-    /**
-     * Clean up after running each test case
-     */
-    public function tearDown()
-    {
-    }
-
-    /**
-     * Clean up after running all test cases
-     */
-    public static function tearDownAfterClass()
-    {
-    }
-
     /**
      * Test "SendEmailToMultipleRecipientsRequest"
      */
